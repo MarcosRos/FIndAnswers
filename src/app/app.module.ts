718dea519f0a40ea6ad60b6cec64f5c7f82d718d
AugustoFlores1528 <82318927+AugustoFlores1528@users.noreply.github.com>
2022-11-11 00:16:15 -0300

    2 files modified, 27 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BodyComponent } from './components/body/body.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, HeaderComponent],
+  declarations: [AppComponent, HelloComponent, HeaderComponent, BodyComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
