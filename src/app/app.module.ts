51e65441c8ffd53e6d1e8e1974a0abe6e8e91c96
MarcosRos <80920805+MarcosRos@users.noreply.github.com>
2022-11-11 02:21:29 -0300

    6 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './components/header/header.component';
 import { AppComponent } from './app.component';
+import { UserService } from './services/user.service';
 import { HelloComponent } from './hello.component';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHandler,
+} from '@angular/common/http';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   declarations: [AppComponent, HelloComponent, HeaderComponent],
+  providers: [UserService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  resultado!: any;
+  constructor(private us: UserService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  obtenerUsuarios() {
+    this.us.getUsuarios().subscribe(
+      (result) => {
+        this.resultado = result;
+        console.log(this.resultado);
+      },
+      (error) => {
+        console.log(error);
+        alert('Error en la petición');
+      }
+    );
   }
 
+  ngOnInit() {}
 }
